4563349db80e414ebb033e122279df988f364577
Jorgemouse <georgemouse1@gmail.com>
2012-05-30 22:59:49 +0800

    rearrange the a-b pruning code, making it more clear

--- alpha_beta_pruning.pl
+++ alpha_beta_pruning.pl
@@ -43,16 +43,10 @@ alpha_beta_pruning(State, Depth, Color, NewState, Value):-
  */
 alpha_beta_pruning(_, State, _, State, Value, _, _) :- final(State, Value),!.
 
-/**
- * Relation: alpha_beta_pruning/7
- * Searches for a move using the alpha-beta pruning algorithm with an alpha and a beta value
- */
+
 alpha_beta_pruning(0, State, _, State, Value, _, _) :- eval(State, Value),!.
 
-/**
- * Relation: alpha_beta_pruning/7
- * Searches for a move using the alpha-beta pruning algorithm with an alpha and a beta value
- */
+
 alpha_beta_pruning(Depth, State, Color, NewState, Value, Alpha, Beta) :-
 	Depth > 0,
 	garbage_collect,
@@ -66,7 +60,28 @@ alpha_beta_pruning(Depth, State, Color, NewState, Value, Alpha, Beta) :-
 		alpha_beta_pruning(StatesList, NDepth, Color, RivalColor, NewState, Value, Alpha, Beta),
 		_,
 		alpha_beta_pruning_recover(StatesList, Depth, Color, RivalColor, NewState, Value, Alpha, Beta)).
+/**
+ * Relation: alpha_beta_pruning/8
+ * Searches for a move using the alpha-beta pruning algorithm with an alpha and a beta value
+ */
+alpha_beta_pruning([State], Depth, _, RivalColor, State, Value, Alpha, Beta):- !, 
+    alpha_beta_pruning(Depth, State, RivalColor, _, Value, Alpha, Beta).
 
+alpha_beta_pruning([State|Rest], Depth, Color, RivalColor, NewState, Value, Alpha, Beta) :-
+    alpha_beta_pruning(Depth, State, RivalColor, _, X, Alpha, Beta),
+    (
+        prune(Color, X, Alpha, Beta) ->
+        (
+            NewState = State,
+            Value is X
+        );
+        (
+            recalc(Color, X, Alpha, Beta, Nalpha, NBeta),
+            alpha_beta_pruning(Rest, Depth, Color, RivalColor, B, Y, Nalpha, NBeta),
+            best(Color, X, Y, State, B, NewState, Value)
+        )
+        
+    ).
 /**
  * Relation: alpha_beta_pruning_recover/8
  * Used to recover when an exception occurs. Updates the depth to 0 to end the search
@@ -85,40 +100,6 @@ alpha_beta_pruning_recover(StatesList, Depth, Color, RivalColor, NewState, Value
 	garbage_collect,
 	alpha_beta_pruning(StatesList, 0, Color, RivalColor, NewState, Value, Alpha, Beta).
 
-/**
- * Relation: alpha_beta_pruning/8
- * Searches for a move using the alpha-beta pruning algorithm with an alpha and a beta value
- * @1: StatesList - list of possible states
- * @2: Depth - the maximum depth of the search
- * @3: Color - the color of the player that moves next
- * @4: RivalColor - the color of the rival
- * @5: NewState - the board after applying the move selected by the search
- * @6: Value - the heuristic value of the move
- * @7: Alpha - the current best value for the player that tries to minimize the game value
- * @8: Beta - the current best value for the player that tries to maximize the game value
- */
-alpha_beta_pruning([State], Depth, _, RivalColor, State, Value, Alpha, Beta):- !, 
-	alpha_beta_pruning(Depth, State, RivalColor, _, Value, Alpha, Beta).
-
-/**
- * Relation: alpha_beta_pruning_recover/8
- * Searches for a move using the alpha-beta pruning algorithm with an alpha and a beta value
- */
-alpha_beta_pruning([State|Rest], Depth, Color, RivalColor, NewState, Value, Alpha, Beta) :-
-	alpha_beta_pruning(Depth, State, RivalColor, _, X, Alpha, Beta),
-	(
-		prune(Color, X, Alpha, Beta) ->
-		(
-			NewState = State,
-			Value is X
-		);
-		(
-			recalc(Color, X, Alpha, Beta, Nalpha, NBeta),
-			alpha_beta_pruning(Rest, Depth, Color, RivalColor, B, Y, Nalpha, NBeta),
-			best(Color, X, Y, State, B, NewState, Value)
-		)
-		
-	).
 
 /**
  * Relation: prune/4
@@ -131,10 +112,6 @@ alpha_beta_pruning([State|Rest], Depth, Color, RivalColor, NewState, Value, Alph
 prune(black, Value, _, Beta):-
 	Value >= Beta.
 
-/**
- * Relation: prune/4
- * Succeds if a prune has to be done
- */
 prune(white, Value, Alpha, _):-
 	Value =< Alpha.
 
@@ -151,10 +128,6 @@ prune(white, Value, Alpha, _):-
 recalc(black, Value, Alpha, Beta, Nalpha, Beta):-
 	max_list([Alpha, Value], Nalpha).
 
-/**
- * Relation: recalc/6
- * Recalculates alpha and beta values
- */
 recalc(white, Value, Alpha, Beta, Alpha, NBeta):-
 	min_list([Beta, Value], NBeta).
 
