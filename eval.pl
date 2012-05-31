3c879f8351caa1e9a199319eff2d82cd70801237
mingo <ipod825@gmail.com>
2012-05-31 22:43:10 +0800

    merge with george

--- eval.pl
+++ eval.pl
@@ -4,17 +4,27 @@ final(Board, Value):-
     Value is BlackPieces - WhitePieces.
 
 eval(Caller,Board, Value):-
-    count_pieces(black, Board, BlackPieces, WhitePieces),
-    HeuristicValue1 is BlackPieces - WhitePieces,
-	getCorners(Corners),
-	positionCount(black,Board,Corners,BlackCorner,WhiteCorner),
-	Bonus is 10*(BlackCorner-WhiteCorner),
+	(
+		Caller=black->
+			count_pieces(black, Board, BlackPieces, WhitePieces),
+			HeuristicValue1 is BlackPieces - WhitePieces,
+			getCorners(Corners),
+			positionCount(black,Board,Corners,BlackCorner,WhiteCorner),
+			Bonus is 10*(BlackCorner-WhiteCorner)
+		;
+		Caller=white->
+			count_pieces(black, Board, BlackPieces, WhitePieces),
+			HeuristicValue1 is BlackPieces - WhitePieces,
+			getCorners(Corners),
+			positionCount(black,Board,Corners,BlackCorner,WhiteCorner),
+			Bonus is 10*(BlackCorner-WhiteCorner)
 /*
     valid_positions(Board, black, BlackValidMoves),
     valid_positions(Board, white, WhiteValidMoves),
     HeuristicValue2 is BlackValidMoves - WhiteValidMoves,
     max_list([HeuristicValue1,HeuristicValue2], Value).
 */
+	),
 	Value is HeuristicValue1+Bonus.
 
 
